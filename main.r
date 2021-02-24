7c1ef7373559bfbcb21e1f4be394ddacc5ed9782
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2021-02-24 11:47:20 -0800

    Update main.r

--- main.r
+++ main.r
@@ -1,3 +1,5 @@
-# My first program in R Programming
-myString <- "Hello, World!
+# My first program in R Programming - inline comment
+ "My first program in R Programming - block comment"
+
+myString <- "Hello, World!"
 print ( myString)
